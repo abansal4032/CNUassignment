214e7600462f56a0308c7fa6a4e1062dc46aaa2b
abansal4032 <archit.bansal18@gmail.com>
2016-07-09 19:25:05 +0400

    Update ProductController.java

--- Assignment04/src/main/java/com/cnu2016/assignment04/Controllers/ProductController.java
+++ Assignment04/src/main/java/com/cnu2016/assignment04/Controllers/ProductController.java
@@ -15,7 +15,7 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @RequestMapping(value = "api/product", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/products", method = RequestMethod.GET)
     public ResponseEntity productAllGet() {
         Iterable<product> result =  productRepository.findByEnabled(1);
         if(result != null) {
@@ -26,7 +26,7 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
-    @RequestMapping(value = "api/product/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/products/{id}", method = RequestMethod.GET)
     public ResponseEntity productOneGet(@PathVariable("id") Integer id) {
         if(id == null){
             Map<String, String> response = new HashMap<String, String>();
@@ -43,7 +43,7 @@ public class ProductController {
 
     }
 
-    @RequestMapping(value = "api/product", method = RequestMethod.POST)
+    @RequestMapping(value = "/api/products", method = RequestMethod.POST)
     public ResponseEntity productAddPost(@RequestBody product Product) {
         if(Product.getCode() == null) {
             Map<String, String> response = new HashMap<String, String>();
@@ -54,7 +54,7 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(Product);
     }
 
-    @RequestMapping(value = "api/product/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/api/products/{id}", method = RequestMethod.PUT)
     public ResponseEntity productAddPut(@RequestBody product Product, @PathVariable("id") Integer id) {
         if(id == null || Product.getCode() == null) {
             Map<String, String> response = new HashMap<String, String>();
@@ -74,7 +74,7 @@ public class ProductController {
         }
     }
 
-    @RequestMapping(value = "api/product/{id}", method = RequestMethod.PATCH)
+    @RequestMapping(value = "/api/products/{id}", method = RequestMethod.PATCH)
     public ResponseEntity productAddPatch(@RequestBody product Product, @PathVariable("id") Integer id) {
         if(id == null) {
             Map<String, String> response = new HashMap<String, String>();
@@ -94,7 +94,7 @@ public class ProductController {
         }
     }
 
-    @RequestMapping(value = "api/product/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/api/products/{id}", method = RequestMethod.DELETE)
     public ResponseEntity productDelete(@PathVariable("id") Integer id) {
         if(id == null) {
             Map<String, String> response = new HashMap<String, String>();
